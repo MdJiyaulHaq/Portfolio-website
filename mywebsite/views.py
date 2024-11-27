9ed780b076f0009a7bcd08c57f06c901528bddc1
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2024-11-27 11:40:26 +0545

    render html template as response

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "index.html")
+
 
 def aboutUs(request):
     return HttpResponse("Welcome to my website.")
 
+
 def course(request):
     return HttpResponse("these are the courses")
 
-def courseDetails(request,courseId):
+
+def courseDetails(request, courseId):
     return HttpResponse(courseId)
