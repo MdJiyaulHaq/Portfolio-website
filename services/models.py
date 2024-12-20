6fd05549c75e6a64d0248df3b39a06863c8c2a54
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2024-12-20 11:34:49 +0545

    installing tinymce editor

--- services/models.py
+++ services/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 # Create your models here.
 class Services(models.Model):
     service_icon = models.CharField(max_length=255)
     service_title = models.CharField(max_length=255)
-    service_des = models.TextField()
+    service_des = HTMLField()
