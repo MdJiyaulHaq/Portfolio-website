28da12e8965274588eeb84b3c0bf4f1ed60cdc25
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2024-12-20 16:06:23 +0545

    marquee stopping on mouseover

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -5,17 +5,8 @@ from services.models import Services
 
 
 def home(request):
-    data = {
-        "title": "Home Page",
-        "plist": ["Java", "Python", "Django"],
-        "students": [
-            {"name": "Brajesh", "phone": 9834543345},
-            {"name": "Bikram", "phone": 9857543345},
-            {"name": "Shankar", "phone": 9865543345},
-            {"name": "Hari", "phone": 9807543345},
-        ],
-        "numbers": [1, 2, 3, 4, 5, 6],
-    }
+    servicesData = Services.objects.all().order_by("-service_title")
+    data = {"servicesData": servicesData}
     return render(request, "index.html", data)
 
 
